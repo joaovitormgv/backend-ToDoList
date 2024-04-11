28517899711fd928035cd75460708f5aa91d22d7
João <joaovitormgv05@gmail.com>
2024-04-10 21:12:08 -0300

    criar sql/setup.sql

---
+++ sql/setup.sql
@@ -0,0 +1,16 @@
+-- Descrição: Este arquivo contém os comandos SQL para criar o banco de dados e tabelas.
+CREATE DATABASE todolist;
+
+\c todolist;
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ToDos (
+    userId INT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN NOT NULL
+);
